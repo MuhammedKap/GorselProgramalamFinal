aea9a28e93f1e34471656ae09b52f9e524a33cec
MuhammedKap <muhammedkap5353@gmail.com>
2025-06-02 17:10:00 +0300

    Add files via upload

---
+++ GYM ÜYE KAYIT SİSTEMİ/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace GYM_ÜYE_KAYIT_SİSTEMİ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl_girisekrani = new System.Windows.Forms.Panel();
+            this.lbl_sporsalonusistemi = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_cikisyapx = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_kullaniciadi = new System.Windows.Forms.Label();
+            this.txt_kullaniciadi = new System.Windows.Forms.TextBox();
+            this.lbl_sifre = new System.Windows.Forms.Label();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.btn_kapat = new System.Windows.Forms.Button();
+            this.pnl_girisekrani.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_girisekrani
+            // 
+            this.pnl_girisekrani.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(19)))), ((int)(((byte)(33)))));
+            this.pnl_girisekrani.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnl_girisekrani.Controls.Add(this.lbl_sporsalonusistemi);
+            this.pnl_girisekrani.Controls.Add(this.pictureBox1);
+            this.pnl_girisekrani.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_girisekrani.Location = new System.Drawing.Point(0, 0);
+            this.pnl_girisekrani.Name = "pnl_girisekrani";
+            this.pnl_girisekrani.Size = new System.Drawing.Size(226, 400);
+            this.pnl_girisekrani.TabIndex = 0;
+            // 
+            // lbl_sporsalonusistemi
+            // 
+            this.lbl_sporsalonusistemi.AutoSize = true;
+            this.lbl_sporsalonusistemi.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sporsalonusistemi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_sporsalonusistemi.Location = new System.Drawing.Point(3, 291);
+            this.lbl_sporsalonusistemi.Name = "lbl_sporsalonusistemi";
+            this.lbl_sporsalonusistemi.Size = new System.Drawing.Size(210, 18);
+            this.lbl_sporsalonusistemi.TabIndex = 1;
+            this.lbl_sporsalonusistemi.Text = "Spor Salonu Üyelik Sistemi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(63, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_cikisyapx
+            // 
+            this.lbl_cikisyapx.AutoSize = true;
+            this.lbl_cikisyapx.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_cikisyapx.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_cikisyapx.Location = new System.Drawing.Point(553, 0);
+            this.lbl_cikisyapx.Name = "lbl_cikisyapx";
+            this.lbl_cikisyapx.Size = new System.Drawing.Size(20, 22);
+            this.lbl_cikisyapx.TabIndex = 1;
+            this.lbl_cikisyapx.Text = "X";
+            this.lbl_cikisyapx.Click += new System.EventHandler(this.lbl_cikisyapx_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Giris Hesabı";
+            // 
+            // lbl_kullaniciadi
+            // 
+            this.lbl_kullaniciadi.AutoSize = true;
+            this.lbl_kullaniciadi.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_kullaniciadi.Location = new System.Drawing.Point(255, 117);
+            this.lbl_kullaniciadi.Name = "lbl_kullaniciadi";
+            this.lbl_kullaniciadi.Size = new System.Drawing.Size(100, 21);
+            this.lbl_kullaniciadi.TabIndex = 3;
+            this.lbl_kullaniciadi.Text = "Kullanıcı Adı";
+            // 
+            // txt_kullaniciadi
+            // 
+            this.txt_kullaniciadi.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_kullaniciadi.Location = new System.Drawing.Point(259, 141);
+            this.txt_kullaniciadi.Multiline = true;
+            this.txt_kullaniciadi.Name = "txt_kullaniciadi";
+            this.txt_kullaniciadi.Size = new System.Drawing.Size(261, 30);
+            this.txt_kullaniciadi.TabIndex = 0;
+            // 
+            // lbl_sifre
+            // 
+            this.lbl_sifre.AutoSize = true;
+            this.lbl_sifre.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sifre.Location = new System.Drawing.Point(255, 185);
+            this.lbl_sifre.Name = "lbl_sifre";
+            this.lbl_sifre.Size = new System.Drawing.Size(43, 21);
+            this.lbl_sifre.TabIndex = 3;
+            this.lbl_sifre.Text = "Şifre";
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_sifre.Location = new System.Drawing.Point(259, 209);
+            this.txt_sifre.Multiline = true;
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.PasswordChar = '*';
+            this.txt_sifre.Size = new System.Drawing.Size(261, 30);
+            this.txt_sifre.TabIndex = 1;
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(19)))), ((int)(((byte)(33)))));
+            this.btn_giris.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_giris.FlatAppearance.BorderSize = 0;
+            this.btn_giris.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(19)))), ((int)(((byte)(33)))));
+            this.btn_giris.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(19)))), ((int)(((byte)(33)))));
+            this.btn_giris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_giris.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_giris.ForeColor = System.Drawing.Color.White;
+            this.btn_giris.Location = new System.Drawing.Point(259, 301);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(93, 34);
+            this.btn_giris.TabIndex = 2;
+            this.btn_giris.Text = "GİRİS";
+            this.btn_giris.UseVisualStyleBackColor = false;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // btn_kapat
+            // 
+            this.btn_kapat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(19)))), ((int)(((byte)(33)))));
+            this.btn_kapat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_kapat.FlatAppearance.BorderSize = 0;
+            this.btn_kapat.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(19)))), ((int)(((byte)(33)))));
+            this.btn_kapat.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(19)))), ((int)(((byte)(33)))));
+            this.btn_kapat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_kapat.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_kapat.ForeColor = System.Drawing.Color.White;
+            this.btn_kapat.Location = new System.Drawing.Point(414, 301);
+            this.btn_kapat.Name = "btn_kapat";
+            this.btn_kapat.Size = new System.Drawing.Size(93, 34);
+            this.btn_kapat.TabIndex = 3;
+            this.btn_kapat.Text = "KAPAT";
+            this.btn_kapat.UseVisualStyleBackColor = false;
+            this.btn_kapat.Click += new System.EventHandler(this.btn_kapat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(575, 400);
+            this.Controls.Add(this.btn_kapat);
+            this.Controls.Add(this.btn_giris);
+            this.Controls.Add(this.txt_sifre);
+            this.Controls.Add(this.lbl_sifre);
+            this.Controls.Add(this.txt_kullaniciadi);
+            this.Controls.Add(this.lbl_kullaniciadi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_cikisyapx);
+            this.Controls.Add(this.pnl_girisekrani);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.pnl_girisekrani.ResumeLayout(false);
+            this.pnl_girisekrani.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_girisekrani;
+        private System.Windows.Forms.Label lbl_cikisyapx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_kullaniciadi;
+        private System.Windows.Forms.TextBox txt_kullaniciadi;
+        private System.Windows.Forms.Label lbl_sifre;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Button btn_giris;
+        private System.Windows.Forms.Button btn_kapat;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_sporsalonusistemi;
+    }
+}
+
---
+++ GYM ÜYE KAYIT SİSTEMİ/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GYM_ÜYE_KAYIT_SİSTEMİ
+{
+    public partial class Form1 : Form
+    {
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_cikisyapx_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_kapat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_giris_Click(object sender, EventArgs e)
+        {
+            string kullaniciadi, sifre = "";
+
+            kullaniciadi = txt_kullaniciadi.Text;
+            sifre = txt_sifre.Text;
+
+            if (kullaniciadi == "" || sifre == "")
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre boş bırakılamaz", "Hatalı giriş denemsi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            else
+            {
+
+
+                if (kullaniciadi == "1" && sifre == "1")
+                {
+
+
+                    AdminSayfasi adminForm = new AdminSayfasi();
+
+                    adminForm.Show();
+                    this.Hide();
+
+                }
+
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı veya şifre hatalı", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+    }
+    
+}
